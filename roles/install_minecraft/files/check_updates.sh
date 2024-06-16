620b474de3a064892b2a1b3f77dafd318fffe4c3
Carnivuth <matti200042@gmail.com>
2024-06-16 21:39:49 +0200

    removed unused roles

--- roles/install_minecraft/files/check_updates.sh
+++
@@ -1,8 +0,0 @@
-MINECRAFT_FOLDER='/usr/local/minecraft'
-latest_version="$(curl -s 'https://launchermeta.mojang.com/mc/game/version_manifest.json' | jq -r  '.versions[0].id')"
-if [[ "$(ls $MINECRAFT_FOLDER/versions | grep $latest_version)" == '' ]]; then
-    echo 'server da aggiornare'
-else
-    echo 'server aggiornato'
-fi
-
