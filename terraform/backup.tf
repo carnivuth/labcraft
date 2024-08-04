da7f7974ae9344b6dd4938acef2df876abb474f7
carnivuth <matti200042@gmail.com>
2024-08-04 22:06:35 +0200

    removed develop machine, changed backupper to a container

--- terraform/backup.tf
+++ terraform/backup.tf
@@ -1,36 +1,25 @@
-resource "proxmox_vm_qemu" "ditto" {
-  name        = "ditto"
-  tags        = "backupper"
-  cores       = 2
-  sockets     = 1
-  memory      = 4096
-  target_node = var.proxmox_host
-  clone       = "ubuntu-2404-cloudinit-template"
-  ipconfig0   = "gw=192.168.1.1,ip=192.168.1.93/24"
-  scsihw      = "virtio-scsi-pci"
-  boot        = "order=scsi0;net0"
-  cipassword  = var.guest_password
-  ciuser      = var.guest_user
-  sshkeys     = var.ssh_pub_key
-  nameserver  = var.nameserver
-  onboot      = true
-  vm_state    = "running"
+resource "proxmox_lxc" "ditto" {
+  target_node     = var.proxmox_host
+  hostname        = "ditto"
+  tags            = "backupper"
+  ostemplate      = var.prod_ct_template
+  password        = var.guest_password
+  unprivileged    = true
+  ssh_public_keys = var.ssh_pub_key
+  nameserver      = var.external_nameserver
+  cores           = 1
+  memory          = 512
+  onboot          = true
+  start           = true
 
-  disks {
-    ide {
-      ide2 {
-        cloudinit {
-          storage = var.main_pool
-        }
-      }
-    }
-    scsi {
-      scsi0 {
-        disk {
-          size    = "500G"
-          storage = var.main_pool
-        }
-      }
-    }
+  rootfs {
+    storage = var.main_pool
+    size    = "40G"
+  }
+  network {
+    name   = "eth0"
+    bridge = "vmbr0"
+    ip     = "192.168.1.93/24"
+    gw     = var.guest_gw
   }
 }
