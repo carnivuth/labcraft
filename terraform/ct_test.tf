a91ec75900cff7c55a22ad73061628679ca863e5
carnivuth <matti200042@gmail.com>
2024-09-29 15:59:10 +0000

    general bugfixing

--- terraform/ct_test.tf
+++ terraform/ct_test.tf
@@ -1,7 +1,33 @@
+//resource "proxmox_lxc" "asdrubale" {
+//  target_node     = var.proxmox_host
+//  hostname        = "asdrubale"
+//  tags            = "vpn;test;reverse_proxy;grocy"
+//  ostemplate      = var.prod_ct_template
+//  password        = var.guest_password
+//  unprivileged    = true
+//  ssh_public_keys = var.ssh_pub_key
+//  nameserver      = var.external_nameserver
+//  cores           = 1
+//  memory          = 512
+//  onboot          = true
+//  start           = true
+//  //hookscript      = "local:snippets/update.sh"
+//
+//  rootfs {
+//    storage = var.main_pool
+//    size    = "8G"
+//  }
+//  network {
+//    name   = "eth0"
+//    bridge = "vmbr0"
+//    ip     = "192.168.1.202/24"
+//    gw     = var.guest_gw
+//  }
+//}
 resource "proxmox_lxc" "ct-test" {
   target_node     = var.proxmox_host
   hostname        = "ct-test"
-  tags            = "test;reverse_proxy;grocy"
+  tags            = "vpn;test;reverse_proxy;grocy"
   ostemplate      = var.prod_ct_template
   password        = var.guest_password
   unprivileged    = true
