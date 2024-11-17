619032f24cff64dc6c7b8e337b0edd5a8f21c1b6
carnivuth <matti200042@gmail.com>
2024-11-17 16:05:03 +0100

    remove dns test

--- terraform/ct_test.tf
+++ terraform/ct_test.tf
@@ -1,40 +1,40 @@
-locals{
-  dns_servers = [
-    {
-        name = "espeon"
-        address = "96"
-      },
-    {
-        name = "umbreon"
-        address = "97"
-      },
-  ]
-}
-resource "proxmox_lxc" "dns-test" {
-  count = 2
-  target_node     = var.proxmox_host
-  hostname        = "ct-test-${local.dns_servers[count.index].name}"
-  tags            = "test"
-  ostemplate      = var.prod_ct_template
-  password        = var.guest_password
-  unprivileged    = true
-  ssh_public_keys = var.ssh_pub_key
-  nameserver      = var.external_nameserver
-  cores           = 1
-  pool           = "test"
-  memory          = 512
-  onboot          = true
-  start           = true
-  //hookscript      = "local:snippets/update.sh"
-
-  rootfs {
-    storage = var.main_pool
-    size    = "8G"
-  }
-  network {
-    name   = "eth0"
-    bridge = "vmbr0"
-    ip     = "192.168.1.${local.dns_servers[count.index].address}/24"
-    gw     = var.guest_gw
-  }
-}
+//locals{
+//  dns_servers = [
+//    {
+//        name = "espeon"
+//        address = "201"
+//      },
+//    {
+//        name = "umbreon"
+//        address = "202"
+//      },
+//  ]
+//}
+//resource "proxmox_lxc" "dns-test" {
+//  count = 2
+//  target_node     = var.proxmox_host
+//  hostname        = "ct-test-${local.dns_servers[count.index].name}"
+//  tags            = "test"
+//  ostemplate      = var.prod_ct_template
+//  password        = var.guest_password
+//  unprivileged    = true
+//  ssh_public_keys = var.ssh_pub_key
+//  nameserver      = var.external_nameserver
+//  cores           = 1
+//  pool           = "test"
+//  memory          = 512
+//  onboot          = true
+//  start           = true
+//  //hookscript      = "local:snippets/update.sh"
+//
+//  rootfs {
+//    storage = var.main_pool
+//    size    = "8G"
+//  }
+//  network {
+//    name   = "eth0"
+//    bridge = "vmbr0"
+//    ip     = "192.168.1.${local.dns_servers[count.index].address}/24"
+//    gw     = var.guest_gw
+//  }
+//}
