198a1b89347713830c44db162f165b59d04db968
carnivuth <matti200042@gmail.com>
2024-11-17 15:05:03 +0100

    test loops

--- terraform/ct_test.tf
+++ terraform/ct_test.tf
@@ -1,6 +1,7 @@
 resource "proxmox_lxc" "ct-test" {
+  count = 14
   target_node     = var.proxmox_host
-  hostname        = "ct-test"
+  hostname        = "ct-test-${count.index}"
   tags            = "vpn;test;reverse_proxy;grocy"
   ostemplate      = var.prod_ct_template
   password        = var.guest_password
@@ -21,7 +22,7 @@ resource "proxmox_lxc" "ct-test" {
   network {
     name   = "eth0"
     bridge = "vmbr0"
-    ip     = "192.168.1.200/24"
+    ip     = "192.168.1.20${count.index}/24"
     gw     = var.guest_gw
   }
 }
