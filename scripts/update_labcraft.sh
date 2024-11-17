5cfde635998316f0106d1987e8b8a7a773d22293
carnivuth <matti200042@gmail.com>
2024-11-17 14:25:12 +0100

    fixed bug when repo is in conflict

--- scripts/update_labcraft.sh
+++ scripts/update_labcraft.sh
@@ -3,6 +3,9 @@ NTFY_ADDRESS="https://pokelab.ddns.net/ntfy/homelab"
 LOG_DIR="/var/log"; if [[ ! -d "$LOG_DIR" ]];then mkdir -p "$LOG_DIR"; fi
 
 cd /usr/local/labcraft
-if ! git pull | grep -E 'Already up to date.' >> "$LOG_DIR/$(basename "$0").log"; then
-        curl  "$NTFY_ADDRESS" -d "updated labcraft repo";
+output="$(git pull)"
+if echo "$output" | grep -E 'Aborted' >> "$LOG_DIR/$(basename "$0").log"; then
+  curl  "$NTFY_ADDRESS" -d "there are conflicts in repo";
+elif ! echo "$output" | grep -E 'Already up to date.' >> "$LOG_DIR/$(basename "$0").log"; then
+  curl  "$NTFY_ADDRESS" -d "updated labcraft repo";
 fi
