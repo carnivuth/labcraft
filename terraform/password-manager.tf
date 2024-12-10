562b38f46f74e37e26243d392c2eb0acf4e5a1c4
carnivuth <matti200042@gmail.com>
2024-12-10 14:37:00 +0100

    added pwmanager container

---
+++ terraform/password-manager.tf
@@ -0,0 +1,27 @@
+
+resource "proxmox_lxc" "klefky" {
+  target_node     = var.proxmox_host
+  hostname        = "klefky"
+  tags        = "pw-manager"
+  ostemplate      = var.prod_ct_template
+  password        = var.guest_password
+  unprivileged    = true
+  ssh_public_keys = var.ssh_pub_key
+  nameserver      = var.nameserver
+  cores           = 2
+  memory          = 2048
+  onboot          = true
+  start           = true
+
+  rootfs {
+    storage = var.main_pool
+    size    = "15G"
+  }
+
+  network {
+    name   = "eth0"
+    bridge = "vmbr0"
+    ip     = "192.168.1.91/24"
+    gw     = var.guest_gw
+  }
+}
