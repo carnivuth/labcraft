8b3138082a93320957378bc5771e77f4eeffd550
carnivuth <matti200042@gmail.com>
2024-12-12 22:38:32 +0100

    restored prod added pool

--- terraform/backup.tf
+++ terraform/backup.tf
@@ -3,6 +3,7 @@ resource "proxmox_lxc" "ditto" {
   target_node     = var.proxmox_host
   hostname        = "ditto"
   tags            = "backupper"
+  pool        = "prod"
   ostemplate      = var.prod_ct_template
   password        = var.guest_password
   unprivileged    = true
