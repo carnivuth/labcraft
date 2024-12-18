e3fc59ed244c54d8b9dd7a6f9db7668f0f73326e
Matteo Longhi <mlonghi@labs.it>
2024-12-18 10:08:29 +0100

    added sanet test

--- terraform/ct_test.tf
+++ terraform/ct_test.tf
@@ -65,30 +65,30 @@
 //    gw     = var.guest_gw
 //  }
 //}
-//resource "proxmox_lxc" "sanet-test" {
-//  target_node     = var.proxmox_host
-//  hostname        = "sanet-test"
-//  tags            = "test"
-//  ostemplate      = "local:vztmpl/sanet-x86_64-new.tar.zst"
-//  password        = var.guest_password
-//  unprivileged    = false
-//  ssh_public_keys = var.ssh_pub_key
-//  nameserver      = var.external_nameserver
-//  cores           = 4
-//  pool           = "test"
-//  memory          = 4196
-//  onboot          = true
-//  start           = true
-//  //hookscript      = "local:snippets/update.sh"
-//
-//  rootfs {
-//    storage = var.main_pool
-//    size    = "40G"
-//  }
-//  network {
-//    name   = "eth0"
-//    bridge = "vmbr0"
-//    ip     = "192.168.1.155/24"
-//    gw     = var.guest_gw
-//  }
-//}
+resource "proxmox_lxc" "sanet-test" {
+  target_node     = var.proxmox_host
+  hostname        = "sanet-test"
+  tags            = "test"
+  ostemplate      = "local:vztmpl/sanet-x86_64-new.tar.zst"
+  password        = var.guest_password
+  unprivileged    = false
+  ssh_public_keys = var.ssh_pub_key
+  nameserver      = var.external_nameserver
+  cores           = 4
+  pool           = "test"
+  memory          = 4196
+  onboot          = true
+  start           = true
+  //hookscript      = "local:snippets/update.sh"
+
+  rootfs {
+    storage = var.main_pool
+    size    = "40G"
+  }
+  network {
+    name   = "eth0"
+    bridge = "vmbr0"
+    ip     = "192.168.1.155/24"
+    gw     = var.guest_gw
+  }
+}
