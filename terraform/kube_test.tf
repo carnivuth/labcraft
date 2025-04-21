b9856e19e045e5d73fa198b612c07e27a2b3dc59
carnivuth <matti200042@gmail.com>
2025-04-21 14:06:40 +0200

    added kube cluster

--- terraform/kube_test.tf
+++ terraform/kube_test.tf
@@ -1,38 +1,76 @@
-## VIRTUAL MACHINES
-#resource "proxmox_vm_qemu" "kube" {
-#  name        = "kube"
-#  tags        = "test"
-#  pool        = "test"
-#  cores       = 4
-#  sockets     = 1
-#  memory      = 8192
-#  target_node = var.proxmox_host
-#  clone       = "ubuntu-2404-cloudinit-template"
-#  ipconfig0   = "gw=192.168.1.1,ip=192.168.1.211/24"
-#  scsihw      = "virtio-scsi-pci"
-#  boot        = "order=scsi0;net0"
-#  cipassword  = var.guest_password
-#  ciuser      = var.guest_user
-#  sshkeys     = var.ssh_pub_key
-#  nameserver  = var.nameserver
-#  onboot      = true
-#  vm_state    = "running"
-#
-#  disks {
-#    ide {
-#      ide2 {
-#        cloudinit {
-#          storage = var.main_pool
-#        }
-#      }
-#    }
-#    scsi {
-#      scsi0 {
-#        disk {
-#          size    = "50G"
-#          storage = var.main_pool
-#        }
-#      }
-#    }
-#  }
-#}
+# Control Plane
+resource "proxmox_vm_qemu" "cp" {
+  name        = "cp"
+  tags        = "test"
+  pool        = "test"
+  cores       = 4
+  sockets     = 1
+  memory      = 4192
+  target_node = var.proxmox_host
+  clone       = "ubuntu-2404-cloudinit-template"
+  ipconfig0   = "gw=192.168.1.1,ip=192.168.1.211/24"
+  scsihw      = "virtio-scsi-pci"
+  boot        = "order=scsi0;net0"
+  cipassword  = var.guest_password
+  ciuser      = var.guest_user
+  sshkeys     = var.ssh_pub_key
+  nameserver  = var.nameserver
+  onboot      = true
+  vm_state    = "running"
+
+  disks {
+    ide {
+      ide2 {
+        cloudinit {
+          storage = var.main_pool
+        }
+      }
+    }
+    scsi {
+      scsi0 {
+        disk {
+          size    = "50G"
+          storage = var.main_pool
+        }
+      }
+    }
+  }
+}
+
+resource "proxmox_vm_qemu" "worker" {
+  name        = "cp"
+  tags        = "test"
+  pool        = "test"
+  cores       = 4
+  sockets     = 1
+  memory      = 4192
+  target_node = var.proxmox_host
+  clone       = "ubuntu-2404-cloudinit-template"
+  ipconfig0   = "gw=192.168.1.1,ip=192.168.1.212/24"
+  scsihw      = "virtio-scsi-pci"
+  boot        = "order=scsi0;net0"
+  cipassword  = var.guest_password
+  ciuser      = var.guest_user
+  sshkeys     = var.ssh_pub_key
+  nameserver  = var.nameserver
+  onboot      = true
+  vm_state    = "running"
+
+  disks {
+    ide {
+      ide2 {
+        cloudinit {
+          storage = var.main_pool
+        }
+      }
+    }
+    scsi {
+      scsi0 {
+        disk {
+          size    = "50G"
+          storage = var.main_pool
+        }
+      }
+    }
+  }
+}
