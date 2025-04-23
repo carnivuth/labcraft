992e2f640f272b3f0ea2cdb967b395d0ca23ab00
Matteo Longhi <mlonghi@labs.it>
2025-04-23 17:21:21 +0200

    test

--- terraform/kube_test.tf
+++ terraform/kube_test.tf
@@ -112,40 +112,41 @@ resource "proxmox_vm_qemu" "worker2" {
     }
   }
 }
-resource "proxmox_vm_qemu" "worker3" {
-  name        = "worker3"
-  tags        = "test"
-  pool        = "test"
-  cores       = 4
-  sockets     = 1
-  memory      = 8192
-  target_node = var.proxmox_host
-  clone       = "ubuntu-2404-cloudinit-template"
-  ipconfig0   = "gw=192.168.1.1,ip=192.168.1.214/24"
-  scsihw      = "virtio-scsi-pci"
-  boot        = "order=scsi0;net0"
-  cipassword  = var.guest_password
-  ciuser      = var.guest_user
-  sshkeys     = var.ssh_pub_key
-  nameserver  = var.nameserver
-  onboot      = true
-  vm_state    = "running"
 
-  disks {
-    ide {
-      ide2 {
-        cloudinit {
-          storage = var.main_pool
-        }
-      }
-    }
-    scsi {
-      scsi0 {
-        disk {
-          size    = "50G"
-          storage = var.main_pool
-        }
-      }
-    }
-  }
-}
+#resource "proxmox_vm_qemu" "worker3" {
+#  name        = "worker3"
+#  tags        = "test"
+#  pool        = "test"
+#  cores       = 4
+#  sockets     = 1
+#  memory      = 8192
+#  target_node = var.proxmox_host
+#  clone       = "ubuntu-2404-cloudinit-template"
+#  ipconfig0   = "gw=192.168.1.1,ip=192.168.1.214/24"
+#  scsihw      = "virtio-scsi-pci"
+#  boot        = "order=scsi0;net0"
+#  cipassword  = var.guest_password
+#  ciuser      = var.guest_user
+#  sshkeys     = var.ssh_pub_key
+#  nameserver  = var.nameserver
+#  onboot      = true
+#  vm_state    = "running"
+#
+#  disks {
+#    ide {
+#      ide2 {
+#        cloudinit {
+#          storage = var.main_pool
+#        }
+#      }
+#    }
+#    scsi {
+#      scsi0 {
+#        disk {
+#          size    = "50G"
+#          storage = var.main_pool
+#        }
+#      }
+#    }
+#  }
+#}
