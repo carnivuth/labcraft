7862e9d42d826ebfc6cfa95648f594f74aa76117
Matteo Longhi <mlonghi@labs.it>
2025-04-23 16:36:35 +0200

    removed ntfy notifications

--- scripts/update_labcraft.sh
+++ scripts/update_labcraft.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-NTFY_ADDRESS="https://pokelab.ddns.net/ntfy/homelab"
 LOG_DIR="/var/log"; if [[ ! -d "$LOG_DIR" ]];then mkdir -p "$LOG_DIR"; fi
 
 cd /usr/local/labcraft
 output="$(git pull)"
 if echo "$output" | grep -E 'Aborted' >> "$LOG_DIR/$(basename "$0").log"; then
-  curl  "$NTFY_ADDRESS" -d "there are conflicts in repo";
+        echo -e "Subject: $(basename $0) LOG\n\nthere are conflicts in repo" | sendmail  root
 elif ! echo "$output" | grep -E 'Already up to date.' >> "$LOG_DIR/$(basename "$0").log"; then
-  curl  "$NTFY_ADDRESS" -d "updated labcraft repo";
+        echo -e "Subject: $(basename $0) LOG\n\nupdated labcraft repo" | sendmail  root
 fi
+
