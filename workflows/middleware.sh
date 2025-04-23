dce9799ac25708570a087e253b744b1084b56c82
Matteo Longhi <mlonghi@labs.it>
2025-04-23 17:38:53 +0200

    added deps installation

--- workflows/middleware.sh
+++ workflows/middleware.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 changes="$(git diff-tree --name-only -r HEAD@{1} HEAD)"
+source env/bin/activate
+
+# install python modules and ansible content
+pip install -r requirements.txt
+ansible-galaxy collection install -r collections/requirements.yml
+ansible-galaxy role install -r roles/requirements.yml
 
 for workflow in workflows/*; do
   if [[ "$workflow" != 'workflows/middleware.sh' ]];then
