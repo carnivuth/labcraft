4926518ae5f3f0f1a10db5c670411a94484f4ebc
Matteo Longhi <mlonghi@labs.it>
2025-05-26 16:01:59 +0200

    added traefik test vm

---
+++ terraform/traefik_test.tf
@@ -0,0 +1,29 @@
+resource "proxmox_vm_qemu" "traefik-test" {
+  name        = "traefik-test"
+  tags        = ""
+  pool        = "test"
+  cores       = 1
+  sockets     = 1
+  memory      = 4096
+  target_node = var.proxmox_host
+  clone       = "ubuntu-2404-cloudinit-template"
+  ipconfig0   = "gw=192.168.1.1,ip=192.168.1.94/24"
+  scsihw      = "virtio-scsi-pci"
+  boot        = "order=scsi0;net0"
+  cipassword  = var.guest_password
+  ciuser      = var.guest_user
+  sshkeys     = var.ssh_pub_key
+  nameserver  = var.nameserver
+  onboot      = true
+  vm_state    = "running"
+
+  disks {
+    ide {
+      ide2 {
+        cloudinit {
+          storage = var.main_pool
+        }
+      }
+    }
+  }
+}
