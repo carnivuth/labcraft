6d2d0cba161b5744e38fd234e5fd3152fcfc16b9
Matteo Longhi <mlonghi@labs.it>
2025-06-18 15:11:50 +0200

    destroyed test vm

--- terraform/traefik_test.tf
+++ terraform/traefik_test.tf
@@ -1,26 +1,42 @@
-resource "proxmox_vm_qemu" "traefik-test" {
-  name        = "traefik-test"
-  tags        = ""
+# Control Plane
+resource "proxmox_vm_qemu" "traefik" {
+  name        = "traefik"
+  tags        = "test"
   pool        = "test"
-  cores       = 1
+  cores       = 4
   sockets     = 1
-  memory      = 4096
+  memory      = 4192
   target_node = var.proxmox_host
   clone       = "ubuntu-2404-cloudinit-template"
-  ipconfig0   = "gw=192.168.1.1,ip=192.168.1.230/24"
+  ipconfig0   = "gw=192.168.1.1,ip=192.168.1.211/24"
   scsihw      = "virtio-scsi-pci"
   boot        = "order=scsi0;net0"
   cipassword  = var.guest_password
   ciuser      = var.guest_user
   sshkeys     = var.ssh_pub_key
   nameserver  = var.nameserver
-  onboot      = true
+  onboot      = false
   vm_state    = "running"
+
+  connection {
+    type     = "ssh"
+    user     = "root"
+    password = var.guest_password
+    host     = "192.168.1.211"
+  }
 
   disks {
     ide {
       ide2 {
         cloudinit {
+          storage = var.main_pool
+        }
+      }
+    }
+    scsi {
+      scsi0 {
+        disk {
+          size    = "50G"
           storage = var.main_pool
         }
       }
