9dc68ed4b69134963772264ce951af90cd8f564c
carnivuth <matti200042@gmail.com>
2025-08-26 17:17:19 +0200

    changed to vm

--- infrastructure/IaC/service_cluster.tf
+++ infrastructure/IaC/service_cluster.tf
@@ -1,35 +1,37 @@
-resource "proxmox_lxc" "snorlax" {
-  target_node     = var.proxmox_host
-  hostname        = "snorlax"
-  tags            = "prod;red;nfs"
-  ostemplate      = var.debian_12_ct_template
-  password        = var.guest_password
-  unprivileged    = true
-  ssh_public_keys = var.ssh_pub_key
-  nameserver      = var.external_nameserver
-  cores           = 1
-  memory          = 2048
-  onboot          = true
-  start           = true
+resource "proxmox_vm_qemu" "snorlax" {
+  name        = "snorlax"
+  tags        = "prod;red;nfs"
+  pool        = "prod"
+  cores       = 2
+  sockets     = 1
+  memory      = 2048
+  target_node = var.proxmox_host
+  clone       = "debian-13-cloudinit-template"
+  ipconfig0   = "gw=192.168.1.1,ip=192.168.1.101/24"
+  scsihw      = "virtio-scsi-pci"
+  boot        = "order=scsi0;net0"
+  cipassword  = var.guest_password
+  ciuser      = var.guest_user
+  sshkeys     = var.ssh_pub_key
+  nameserver  = var.nameserver
+  onboot      = true
+  vm_state    = "running"
 
-  rootfs {
-    storage = var.main_pool
-    size    = "8G"
-  }
-  features {
-    nesting = true
-  }
- mountpoint {
-    key     = "0"
-    slot    = 0
-    storage = var.data_pool
-    mp      = "/mnt/"
-    size    = "200G"
-  }
-  network {
-    name   = "eth0"
-    bridge = "vmbr0"
-    ip     = "192.168.1.101/24"
-    gw     = var.guest_gw
+  disks {
+    ide {
+      ide2 {
+        cloudinit {
+          storage = var.main_pool
+        }
+      }
+    }
+    scsi {
+      scsi0 {
+        disk {
+          size    = "200G"
+          storage = var.data_pool
+        }
+      }
+    }
   }
 }
