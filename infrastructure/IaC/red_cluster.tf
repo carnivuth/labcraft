6f6d63ae3adaa41c0427de167782b05a6b54d940
carnivuth <matti200042@gmail.com>
2025-08-26 17:43:23 +0200

    refactor files

--- infrastructure/IaC/red_cluster.tf
+++ infrastructure/IaC/red_cluster.tf
@@ -1,13 +1,13 @@
-resource "proxmox_vm_qemu" "wailord" {
-  name        = "wailord"
-  tags        = "docker;test"
+resource "proxmox_vm_qemu" "snorlax" {
+  name        = "snorlax"
+  tags        = "red;nfs;prod"
   pool        = "prod"
-  cores       = 8
+  cores       = 2
   sockets     = 1
-  memory      = 16384
+  memory      = 2048
   target_node = var.proxmox_host
-  clone       = "ubuntu-2404-cloudinit-template"
-  ipconfig0   = "gw=192.168.1.1,ip=192.168.1.94/24"
+  clone       = "debian-13-cloudinit-template"
+  ipconfig0   = "gw=192.168.1.1,ip=192.168.1.101/24"
   scsihw      = "virtio-scsi-pci"
   boot        = "order=scsi0;net0"
   cipassword  = var.guest_password
@@ -28,8 +28,8 @@ resource "proxmox_vm_qemu" "wailord" {
     scsi {
       scsi0 {
         disk {
-          size    = "400G"
-          storage = var.main_pool
+          size    = "200G"
+          storage = var.data_pool
         }
       }
     }
@@ -38,8 +38,8 @@ resource "proxmox_vm_qemu" "wailord" {
 
 resource "proxmox_vm_qemu" "avalug" {
   name        = "avalug"
-  tags        = "docker;service_manager;prod"
-  pool        = "test"
+  tags        = "docker;service_manager;red;prod"
+  pool        = "prod"
   cores       = 4
   sockets     = 1
   memory      = 8192
@@ -74,6 +74,44 @@ resource "proxmox_vm_qemu" "avalug" {
         disk {
           size    = "500G"
           storage = var.data_pool
+        }
+      }
+    }
+  }
+}
+
+resource "proxmox_vm_qemu" "wailord" {
+  name        = "wailord"
+  tags        = "docker;test"
+  pool        = "prod"
+  cores       = 4
+  sockets     = 1
+  memory      = 8192
+  target_node = var.proxmox_host
+  clone       = "ubuntu-2404-cloudinit-template"
+  ipconfig0   = "gw=192.168.1.1,ip=192.168.1.94/24"
+  scsihw      = "virtio-scsi-pci"
+  boot        = "order=scsi0;net0"
+  cipassword  = var.guest_password
+  ciuser      = var.guest_user
+  sshkeys     = var.ssh_pub_key
+  nameserver  = var.nameserver
+  onboot      = true
+  vm_state    = "running"
+
+  disks {
+    ide {
+      ide2 {
+        cloudinit {
+          storage = var.main_pool
+        }
+      }
+    }
+    scsi {
+      scsi0 {
+        disk {
+          size    = "400G"
+          storage = var.main_pool
         }
       }
     }
